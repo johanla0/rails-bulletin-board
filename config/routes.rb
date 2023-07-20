f85cd323a971d48d7d91d6e4814cb49bb3d863a4
Ivan Dolgov <idlhero@gmail.com>
2023-07-20 08:24:41 +0300

    Add auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   # root "articles#index"
+
+  post 'auth/:provider', to: 'auth#request', as: :auth_request
+  get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 end
