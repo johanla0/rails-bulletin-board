316ee5e7dd9afafb20ee39bb872833ef39af698f
Ivan Dolgov <idlhero@gmail.com>
2023-07-23 14:42:26 +0300

    Add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_135020) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_23_104659) do
+  create_table "categories", force: :cascade do |t|
+    t.string "slug"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
