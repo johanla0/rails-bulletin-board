69eb21208fc4be16de70a95c07339e4ab9057e86
Ivan Dolgov <idlhero@gmail.com>
2023-08-10 04:03:26 +0300

    Fix test

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -75,7 +75,7 @@ class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
 
     patch change_state_bulletin_path(bulletin), params: { state_event: :publish }
 
-    assert_response :redirect
+    assert_response :success
 
     bulletin.reload
 
