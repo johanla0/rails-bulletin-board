641c34c70ddda2fc6c2ffaf5ffb3a324cc4f3922
Ivan Dolgov <idlhero@gmail.com>
2023-08-14 01:51:59 +0300

    Refactor

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer if Rails.env.development?
-  provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: 'user'
+  provider :github, Rails.application.credentials.dig(:github, :client_id), Rails.application.credentials.dig(:github, :client_secret), scope: 'user'
 end
